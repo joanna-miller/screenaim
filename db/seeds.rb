106fda4fc479fa0510e974e186e46ef7eb17df03
Andy Lawson <alawson89@gmail.com>
2021-04-06 13:17:36 -0700

    add a lot of styling
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,114 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
 
 password = 'pass123'
-1.upto(5) do |i|
-  User.create(
-    username: "user-#{i}",
-    email: "user-#{i}@example.com",
-    password: password,
-    password_confirmation: password
-  )
-end
+User.create(
+  username: "FunkyOLips",
+  email: "user-1@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "DrgnGrl4902",
+  email: "user-2@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "crazyandy89",
+  email: "user-3@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "ziggityzack22",
+  email: "user-4@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "thejodotcom",
+  email: "user-5@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "SmarterChild",
+  email: "user-6@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "xXdownWthaClownXx",
+  email: "user-7@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "VioJ72",
+  email: "user-8@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "xX2dopeXx",
+  email: "user-9@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "Roxysurfergurl576",
+  email: "user-10@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "Radskatrboi420",
+  email: "user-11@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "g00dCharlotteLuvr13",
+  email: "user-12@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "limpbizktdude92",
+  email: "user-13@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "wteKnight89",
+  email: "user-14@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "BiggidyMike",
+  email: "user-15@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "xXJusbiaXx",
+  email: "user-16@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "pr1nc3ssP3ach473",
+  email: "user-17@example.com",
+  password: password,
+  password_confirmation: password
+)
+User.create(
+  username: "son1cth3h3dg3",
+  email: "user-18@example.com",
+  password: password,
+  password_confirmation: password
+)
