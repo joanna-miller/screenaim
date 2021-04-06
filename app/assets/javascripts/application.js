1a4fba45de7da4aeb4ffea702515fcaf17657fc0
Andy Lawson <alawson89@gmail.com>
2021-04-06 15:37:10 -0700

    add usernames to message window/ add styling
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,12 @@
 //     }
 //   });
 // });
+
+function myFunction(div) {
+  var x = document.getElementById(div);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
